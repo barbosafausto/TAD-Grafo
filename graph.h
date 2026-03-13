0ef2ee54411185ebbe37f66afe42890024475002
faustus <vitalbfausto@gmail.com>
2026-03-13 10:58:48 -0300

    Initial commit e graph.h

---
+++ graph.h
@@ -0,0 +1,41 @@
+#ifndef GRAFO_H
+    #define GRAFO_H
+
+    #include <stdbool.h>
+    #define int_8 char //Definição de inteiro de 8 bits
+
+    typedef struct grafo GRAFO;
+
+    //Funções abaixo
+
+    //Retorna um grafo de N vértices com 0 arestas.
+    GRAFO* MyGraph(int N); 
+
+    //Criar uma aresta de peso w entre os vértices (u, v).
+    //Retorna True se a operação foi bem-sucedida.
+    bool add_edge(GRAFO* G, int u, int v, int w);
+
+    //Retorna 1 se a aresta existe, 0 caso contrário.
+    int_8 exist_edge(GRAFO* G, int u, int v);
+
+    //Retorna um array (ponteiro) contendo os vizinhos do vértice u.
+    int* neighbors(GRAFO* G, int u);
+
+    //Remove a aresta entre dois vértices (u, v). Retorna True se bem-sucedido.
+    bool remove_edge(GRAFO* G, int u, int v);
+
+    //Imprime todos os vértices e arestas do grafo.
+    void print_info(GRAFO* G);
+
+    //Deleta o grafo, i. e., desaloca toda a memória alocada para ele.
+    //Retorna True se bem sucedido.
+    bool delete_graph(GRAFO** G);
+
+    //Retorna o vértice com maior número de vizinhos (maior grau).
+    int max_neighbors(GRAFO* G);
+
+    //Retorna a matriz de adjacência do grafo (ponteiro de ponteiro).
+    int** adjacency_matrix(GRAFO* G);
+    
+
+#endif
---
+++ main.c
@@ -0,0 +1,15 @@
+#include "graph.h"
+
+/*GRUPO
+15512767 - José Fausto Vital Barbosa
+Aluno 2
+Aluno 3
+Aluno 4
+*/
+
+
+int main() {
+    
+
+    return 0;
+}
